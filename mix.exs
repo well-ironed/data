b4063524f5b2946f04d8c34eb1bcf01472bc601f
Rafał Studnicki <studzien@gmail.com>
2019-10-30 12:50:28 +0100

    Prepare hex.pm publication

--- mix.exs
+++ mix.exs
@@ -4,25 +4,37 @@ defmodule Data.MixProject do
   def project do
     [
       app: :data,
-      version: "0.1.0",
-      elixir: "~> 1.9",
+      deps: deps(),
+      description: "Elixir data structures extensions",
+      docs: docs(),
+      elixir: "~> 1.7",
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      version: "0.1.0"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
+    [{:ex_doc, "~> 0.21.2", only: :dev, runtime: false}]
+  end
+
+  defp docs do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      main: "readme",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/well-ironed/data"}
     ]
   end
 end
