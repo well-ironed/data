795030a4f4f2a4465ed722cd791b4fbcd82b2eef
Rafal Studnicki and Simon Zelazny <developers@well-ironed.com>
2019-11-19 20:10:19 +0000

    add list/2 builtin parser

--- lib/built_in.ex
+++ lib/built_in.ex
@@ -10,4 +10,30 @@ defmodule Data.BuiltIn do
   def integer(_other) do
     Error.domain(:not_an_integer) |> Result.error()
   end
+
+  @spec string(any()) :: Result.t(String.t(), any())
+  def string(s) when is_binary(s) do
+    Result.ok(s)
+  end
+
+  def string(_other) do
+    Error.domain(:not_a_string) |> Result.error()
+  end
+
+  @spec list([a], Data.Parser.parser(a, b)) :: Result.t(b, Error.t) when a: var, b: var
+  def list(xs, p) when is_list(xs) do
+    Result.fold(Result.ok([]), xs, fn el, acc ->
+      case p.(el) do
+        {:ok, parsed} ->
+          Result.ok([parsed|acc])
+        {:error, why} ->
+          why
+          |> Error.map_details(&Map.put(&1, :failed_element, el))
+          |> Result.error()
+      end
+    end)
+    |> Result.map(&Enum.reverse(&1))
+  end
+
+  def list(_, _), do: Error.domain(:not_a_list) |> Result.error()
 end
--- test/built_in_test.exs
+++ test/built_in_test.exs
@@ -9,10 +9,46 @@ defmodule Data.BuiltInTest do
       assert integer(123) == {:ok, 123}
     end
 
-    test "returns an error if something else than integer is passed" do
+    test "returns an error if something other than an integer is passed" do
       assert {:error, error} = integer("123")
       assert Error.kind(error) == :domain
       assert Error.reason(error) == :not_an_integer
     end
+  end
+
+  describe "string/1" do
+    test "returns an error if something other than a binary is passed" do
+      assert {:error, error} = string(123)
+      assert Error.kind(error) == :domain
+      assert Error.reason(error) == :not_a_string
+    end
+
+    test "returns the same string as passed" do
+      assert string("123") == {:ok, "123"}
+    end
+  end
+
+  describe "list/2" do
+    test "successfully parses empty list" do
+      assert list([], &string/1) == {:ok, []}
+    end
+
+    test "successfully parses list where all elements parse" do
+      assert list([1,2,3], &integer/1) == {:ok, [1,2,3]}
+    end
+
+    test "returns error when first argument is not a list" do
+      assert {:error, error} = list(:abc, &integer/1)
+      assert Error.kind(error) == :domain
+      assert Error.reason(error) == :not_a_list
+    end
+
+    test "returns first failed parse if some elements don't parse" do
+      assert {:error, error} = list([1,"2",3], &integer/1)
+      assert Error.kind(error) == :domain
+      assert Error.reason(error) == :not_an_integer
+      assert Error.details(error) == %{failed_element: "2"}
+    end
+
   end
 end
