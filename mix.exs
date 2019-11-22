30d26f555e23dc6f15d770714c05957d518bf575
Rafal Studnicki and Simon Zelazny <developers@well-ironed.com>
2019-11-22 10:19:30 +0000

    Bump to 0.2.1

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Data.MixProject do
       elixir: "~> 1.7",
       package: package(),
       start_permanent: Mix.env() == :prod,
-      version: "0.2.0"
+      version: "0.2.1"
     ]
   end
 
